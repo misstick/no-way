baff31b84fcc29000ed5be0431bb57d2219daf28
Noëlie <noelie@MacBook-Pro-de-Noelie.local>
2013-07-09 00:22:16 +0200

    Change Full Layout: remove right column

--- config.rb
+++ config.rb
@@ -28,7 +28,7 @@ end
 activate :directory_indexes
 page "blog/*", :layout => :blog_layout
 page "blog/content/*", :layout => :detail_layout
-page "/404.html", directory_index: false
+page "/404.html", :directory_index => false
 page "/feed.xml", :layout => false
 
 ###
