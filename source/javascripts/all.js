10fa2aae5d82e14946c6f7bba02ec4870c1a5cd9
Noëlie Amiot <namiot@20minutes.fr>
2013-07-12 16:49:04 +0200

    wall: handle resize & format

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -47,15 +47,6 @@
 	var PictureWall = function(el, options) {
 		if (!options) options = {};
 		this.el = el;
-		var path = ".portrait";
-		this.item = {
-			path: path,
-			coords: this.coords($(path, this.el).first())
-		};
-		this.className = {
-			container: "bloc",
-			single: "one-child"
-		};
 
 		// Init
 		this.cleanMarkup();
@@ -64,6 +55,8 @@
 	}
 
 	PictureWall.prototype = {
+
+		format: ["portrait", "landscape"],
 
 		coords: function(el) {
 			return {
@@ -75,13 +68,13 @@
 		set_grid: function() {
 			this.grid = [];
 			this.el.children().each(function(index, el) {
-				line = Math.round(el.offsetTop / this.item.coords.height);
+				line = Math.round(el.offsetTop / this._coords.height);
 				if (!this.grid[line]) this.grid[line] = [];
 				this.grid[line].push(el);
 			}.bind(this));
 		},
 
-		line_width: function(line) {
+		width: function(line) {
 			var last = _.last(line);
 			return last.offsetLeft + last.offsetWidth;
 		},
@@ -89,6 +82,14 @@
 		stop: function() {
 			this._index = 0;
 			this.el.removeAttr("style");
+		},
+
+
+		nextAll: function(el, value) {
+			var elements = null;
+			var next = $(el).nextAll(this.path_referer()).get(0);
+			if (next) elements = [next];
+			return elements;
 		},
 
 		render: function() {
@@ -103,23 +104,28 @@
 			while(this._index < this.grid.length - 1) {
 				var coords = this.coords(this.el);
 				var line = this.grid[this._index];
-				var width = this.line_width(line);
+				var width = this.width(line);
 
 				// Do not move
 				// if no white space
 				var blank = Math.ceil(coords.width - width);
-				if (blank && blank >= blank_min) {
+				if (blank) {
+
 					var el = _.last(line);
-					var next = $(el).nextAll(this.item.path).get(0);
-					if (next) {
+					var elements = this.nextAll(el);
+
+					if (elements) {
 						// Move into DOM & array
-						$(el).after(next);
+						var elements_width = 0;
+						for (var i=elements.length; i > 0; i--) {
+							var element = elements[i - 1];
+							elements_width += element.offsetWidth;
+							$(el).after(element);
+						}
 
 						// Update Parent Size
-						var size = $(next).width() + width;
-						if (size > coords.width) {
-							this.el.width(size);
-						}
+						var size = elements_width + width;
+						if (size > coords.width) this.el.width(size);
 
 						// Update Context
 						this.set_grid();
@@ -130,30 +136,78 @@
 			}
 		},
 
+		set_format: function(el, coords) {
+			if (!coords) coords = this.coords($(el));
+			var format = (coords.width < coords.height) ? this.format[0] : this.format[1];
+			$(el).attr("data-format", format);
+			return format;
+		},
+
+		get_format: function(el) {
+			return $(el).attr("data-format");
+		},
+
+		path_referer: function() {
+			return "[data-format=" + this.format[0] + "]"
+		},
+
 		cleanMarkup: function() {
 			var bloc = null;
-			var line_height = this.item.coords.height;
-			$("img", this.el).each(function(index, el){
+			var line_height;
+			var className = {
+				container: "bloc",
+				single: "one-child"
+			};
+
+			var index_ref;
+			var pictures = $("img", this.el);
+			pictures.each(function(index, el){
 				var _coords = this.coords($(el));
-				if ($(el).hasClass("landscape")) {
+
+				// Tag each Picture
+				var format = this.set_format(el, _coords);
+
+				// Get The minimal Height
+				// to define line_height
+				if (format === this.format[0]) {
+					if (!line_height || line_height > _coords.height) {
+						index_ref = index;
+						line_height = _coords.height;
+					}
+				}
+			}.bind(this));
+
+			pictures.get(index_ref);
+			this._coords = this.coords($(pictures.get(index_ref)))
+
+			pictures.each(function(index, el){
+				var _coords = this.coords($(el));
+				var format = this.get_format(el);
+
+				if (format === this.format[1]) {
 					var previous = $(el).prev();
-					if (!previous.hasClass(this.className.container) || previous.children().length >= 2) {
-						$(el).before('<div class="' + this.className.container + ' ' + this.className.single + '"></div>');
+					if (!previous.hasClass(className.container) || previous.children().length >= 2) {
+						$(el).before('<div class="' + className.container + ' ' + className.single + '"></div>');
 						previous = $(el).prev();
 					}
-					// Resize Pictures
+					// Resize Landscape
 					var height = line_height / 2;
 					var width = _coords.width * line_height / _coords.height
 					if (previous.children().length) {
 						width = width / 2;
-						previous.removeClass(this.className.single);
+						previous.removeClass(className.single);
 					}
 					previous.height(line_height);
 					previous.width(width);
 
 					// Move Picture
 					previous.append(el);
+					return;
 				}
+				// Resize Portait
+				$(el).width(this._coords.width);
+				$(el).height(this._coords.height);
+
 			}.bind(this));
 			this._content = this.el.html();
 		}
