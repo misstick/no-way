3181495a9ec306fb931ccb4c3957c25c5ef28b7f
Noëlie Amiot <namiot@20minutes.fr>
2013-07-31 12:32:40 +0200

    Set all wall.children to the same size

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -50,9 +50,7 @@
 		this.pictures = $("img", this.el);
 
 		// Init
-		var _func = this.render.bind(this);
-		$(window).on("resize", _.debounce(_func, 100));
-		$(this.el).on("resize", this.resize.bind(this));
+		$(this.el).on("format:end", this.render.bind(this));
 		this.load();
 	}
 
@@ -69,6 +67,7 @@
 			}
 		},
 
+		/*
 		set_grid: function() {
 			this.grid = [];
 			this.el.children().each(function(index, el) {
@@ -77,6 +76,7 @@
 				this.grid[line].push(el);
 			}.bind(this));
 		},
+		*/
 
 		width: function(line) {
 			var last = _.last(line);
@@ -103,55 +103,22 @@
 		},
 
 		load: function() {
-			var loader = _.after(this.pictures.length, this.preload_data.bind(this))
+			var loader = _.after(this.pictures.length, this.format_html.bind(this))
 			this.pictures.each(function(index, el){
 				el.onload = loader;
 			}.bind(this));
 		},
 
+/*
 		render: function() {
+
 			this.stop();
 			// Set the initial content to have the same result
 			// when page will be re-loaded
 			this.el.html(this._content);
 			this.set_grid();
-
-			var blank_min = 0;
-			while(this._index < this.grid.length - 1) {
-				var coords = this.coords(this.el);
-				var line = this.grid[this._index];
-				var width = this.width(line);
-
-
-				// Do not move
-				// if no white space
-				var blank = Math.ceil(coords.width - width);
-				if (blank) {
-
-					var el = _.last(line);
-					var elements = this.nextAll(el, blank);
-					if (elements) {
-
-						// Move into DOM & array
-						var elements_width = 0;
-						for (var i=elements.length; i > 0; i--) {
-							var element = elements[i - 1];
-							elements_width += element.offsetWidth;
-							$(el).after(element);
-						}
-
-						// Update Parent Size
-						var size = Math.ceil(elements_width + width);
-						if (size > coords.width) this.el.width(size);
-
-						// Update Context
-						this.set_grid();
-					}
-				}
-				// Goto Next Line
-				++this._index;
-			}
 		},
+*/
 
 		set_format: function(el, coords) {
 			if (!coords) coords = this.coords($(el));
@@ -168,15 +135,13 @@
 			return "[data-format=" + this.format[0] + "]"
 		},
 
-		preload_data: function() {
+		format_html: function() {
 			var index_ref;
 			var max = Math.ceil(window.innerHeight);
 			this.pictures.each(function(index, el){
 				var _coords = this.coords($(el));
-
 				// Tag each Picture
 				var format = this.set_format(el, _coords);
-
 				// Get The minimal Height
 				// to define line_height
 				if (format === this.format[0]) {
@@ -186,18 +151,16 @@
 					}
 				}
 			}.bind(this));
-
 			var el = $(this.pictures.get(index_ref));
 			if (this.line_height > max) {
 				this.line_height = max;
 				el.width(this.line_height * el.width() / el.height())
 			}
-
 			this._coords = this.coords(el);
-			$(this.el).trigger("resize");
+			$(this.el).trigger("format:end");
 		},
 
-		resize: function() {
+		render: function() {
 			var bloc = null;
 			var className = {
 				container: "bloc",
@@ -206,9 +169,9 @@
 			this.pictures.each(function(index, el){
 				var _coords = this.coords($(el));
 				var format = this.get_format(el);
-
 				if (format === this.format[1]) {
-					var previous = $(el).prev();
+					var previous = $(el).prevAll("." + className.single);
+					if (!previous.get(0)) previous = $(el).prev();
 					if (!previous.hasClass(className.container) || previous.children().length >= 2) {
 						$(el).before('<div class="' + className.container + ' ' + className.single + '"></div>');
 						previous = $(el).prev();
@@ -220,16 +183,18 @@
 						width = width / 2;
 						previous.removeClass(className.single);
 					}
-					previous.height(this.line_height);
-					previous.width(Math.round(width));
+					previous.height(this._coords.height);
+					previous.width(this._coords.width);
 
 					// Move Picture
 					previous.append(el);
 					return;
 				}
+				/*
 				// Resize Portait
 				$(el).width(Math.round(_coords.width * this._coords.height / _coords.height));
 				$(el).height(this._coords.height);
+				*/
 
 			}.bind(this));
 			this._content = this.el.html();
