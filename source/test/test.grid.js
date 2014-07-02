b7db84c8b1de969a64af49e68a692d707d90f499
Noëlie <noelieamiot@gmail.com>
2014-07-02 16:58:33 +0200

    FIX bind/mocha-phantomjs bug

--- source/test/test.grid.js
+++ source/test/test.grid.js
@@ -1,7 +1,6 @@
 describe('Gallery', function(){
+    var view = new PictureWall($("#test-content"));
     
-    // var view = new PictureWall($("#test-content"));
-
     it('load:success', function(done){
       // view.load();
     });
