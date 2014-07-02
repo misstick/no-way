43d6bdf80a5f3e5da10f347167ebb9ad51214fed
Noëlie <noelieamiot@gmail.com>
2014-07-02 16:39:15 +0200

    Review : call cahi in a cleaner way

--- source/test/test.grid.js
+++ source/test/test.grid.js
@@ -1,24 +1,8 @@
-var chai = (typeof require !== "undefined") ? require("chai") : chai;
-var assert = chai.assert;
-var expect = chai.assert;
-
-
-
-/*
-
-TEST CASE
-
-1.
-
-*/
-
 describe('Gallery', function(){
-  var view = new PictureWall($("#test-content"));
-  
-  // it('load:success', function(){
-  //   
-  //   view.load();
-  //   
-  //   
-  // });
+    
+    // var view = new PictureWall($("#test-content"));
+
+    it('load:success', function(done){
+      // view.load();
+    });
 });
