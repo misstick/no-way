18b5696fe42b0606c465d0194ffa518c360f8142
Noëlie <noelieamiot@gmail.com>
2014-07-07 18:32:39 +0200

    Nav : Create methods insteadof private functions

--- source/javascripts/view/nav.js
+++ source/javascripts/view/nav.js
@@ -17,45 +17,48 @@
 	navView.prototype = Object.create(baseView.prototype);
 
 	_.extend(navView.prototype, {
+		
+		template: '<nav data-view="scroll_nav"><button data-action="back"><button data-action="next"></nav>',
 
 		// @TEST : Navigation bar should exist
 		// on nonetouch resolutions
 		render: function() {
-			console.log("menu.render")
-			
-			// Display Visibility
-			var display_buttons = function() {
-				var min = 0;
-				var max = this.el.get(0).scrollWidth - this.el.get(0).offsetWidth;
-				var value = this.el.get(0).scrollLeft;
+			var path_content = '[data-view="scroll_nav"]';
+			if (!$(path_content, this.el).get(0)) {
+				// Add navigation
+				this.el.append(this.template);
+				_.create_affix($("nav", this.el));
 				
-				$("[data-action=next]")[(value === max) ? "addClass" : "removeClass"]("disabled");
-				$("[data-action=back]")[(value === min) ? "addClass" : "removeClass"]("disabled");
+				// Handle Click Event
+				$(path_content + ' button', this.el).on("click", this.goto.bind(this));
 				
-			}.bind(this);
+				// Handle Scroll Event
+				$(this.el).on("scroll", _.throttle(this.display_buttons.bind(this), 100));
+			}
 			
-			var _goto = function(event) {
-				var target = event.currentTarget;
-				var action = $(target).data("action");
-				var step = this.el.width() / 2;
-				var value = (action === "next") ? this.el.get(0).scrollLeft + step : this.el.get(0).scrollLeft - step;
+			console.log(this.cid)
 			
-				// ANimation
-				this.el.animate({ "scrollLeft": value}, { complete: display_buttons});
-			}.bind(this);
+			this.display_buttons();
+		},
+		
+		display_buttons: function() {
+			var min = 0;
+			var max = this.el.get(0).scrollWidth - this.el.get(0).offsetWidth;
+			var value = this.el.get(0).scrollLeft;
 			
-			// Add navigation
-			this.el.append('<nav><button data-action="back"><button data-action="next"></nav>');
-			_.create_affix($("nav", this.el));
+			$("[data-action=next]")[(value === max) ? "addClass" : "removeClass"]("disabled");
+			$("[data-action=back]")[(value === min) ? "addClass" : "removeClass"]("disabled");
+		},
+		
+		goto: function(event) {
+			var target = event.currentTarget;
+			var action = $(target).data("action");
+			var step = this.el.width() / 2;
+			var value = (action === "next") ? this.el.get(0).scrollLeft + step : this.el.get(0).scrollLeft - step;
 			
-			// Handle Click Event
-			$("button", this.el).on("click", _goto);
-			
-			// Handle Scroll Event
-			$(this.el).on("scroll", _.throttle(display_buttons, 100))
-			
-			$(this.el).trigger("scroll");
-		}
+			// ANimation
+			this.el.animate({ "scrollLeft": value}, { complete: display_buttons});
+		},
 	});
 
 	_VIEW[NAMESPACE]  = navView;
