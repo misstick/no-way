470b9b3922bb1a5c123954db674b5c6aa01a0fcf
Noëlie <noelieamiot@gmail.com>
2014-07-07 14:20:33 +0200

    Create file View/scroller

--- source/javascripts/view/gallery.js
+++ source/javascripts/view/gallery.js
@@ -1,4 +1,4 @@
-(function(baseView, loaderView, baseCollection, gridCollection) {
+(function(baseView, loaderView, scrollerView, baseCollection, gridCollection) {
 
 	_.mixin({
 		create_affix: function(el) {
@@ -13,122 +13,7 @@
 			return ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
 		}
 	});
-		
-	var Scroller = function(el, options) {
-		baseView.apply(this, arguments);
-	}
-	
-	Scroller.prototype = Object.create(baseView.prototype);
-	
-	_.extend(Scroller.prototype, {
-		
-		grid: [],
-		
-		initialize: function(el, options) {
-			baseView.prototype.initialize.apply(this, arguments);
-			var _func = this.resize.bind(this);
-			$("body").on("resize", _.debounce(_func, 100));
-		},
-		
-		render: function(html) {
-			if (!this.__content) {
-				$(this.el).html("<div class=scroller></div>");
-				this.__content = $(".scroller", this.el);
-			}
 
-			var content = this.__content;
-			
-			if (_.isString(html)) {
-				
-				// Append new HTML
-				content.html(html)
-
-				// Display columns && rows
-				this.resize();
-			}	
-		},
-		
-		resize: function() {
-			var content = this.__content;
-			
-			// Grid is a table with CID references
-			// of models of collection
-			var grid = this.grid;
-			var collection = this.collection;
-
-			// Remove Previous resize		
-			content.css("width", "auto");
-			
-			var coords = collection.get_item_size({
-				width: window.innerWidth,
-				height: window.innerHeight
-			});
-			
-			var width_max = coords.width * grid.length;
-			var len = width_max / coords.width;
-			
-			// Get Column Value
-			var column_min = Math.ceil(content.width() / coords.width);
-			var columns = column_min;
-			while((len / columns) > 1 && len % columns > 0) {
-				++columns;
-			}
-			var rows = Math.ceil(len / columns);
-			
-			/*
-			if (this._fill === "height") {
-				// Gallery is bigger than window Size
-				// Lets fill it
-				var surface = coords.width * coords.height;
-				var surface_all = surface * len;
-				var surface_win = window.innerHeight * window.innerWidth;
-				if (surface_all > surface_win && row === 1) {
-					columns = column_min;
-					rows = Math.ceil(len / column);
-				}
-			
-				// Add Vertical Alignement
-				var height_min = window.innerHeight;
-				var height = rows * coords.height;
-				this.el.css({
-					"padding": Math.ceil((height_min - height) / 2) + "px 0"
-				});
-			}
-			*/
-			
-			// Force Content.width
-			// to have scroller
-			var width = columns * coords.width;
-			// if (width > width_max) width = width_max;
-			content.width(width);
-			console.log(columns, coords.width, width)
-			
-			// Resize Grid Items
-			// @TODO : add methods
-			_.each(grid, function(data) {
-				_.each(data, function(cid, index) {
-					var item = $('[data-cid=' + cid + ']', this.el);
-					
-					// Resize Container
-					if (!index) {
-						$(item).parent().css(coords);
-					}
-					
-					// Resize Picture
-					var model = _.find(collection.models, function(_model) {
-						return _model.cid == cid;
-					});
-					$(item).css({
-						"background-size": _.template('<%= width %>px <%= height %>px', {
-							width: data.width * item.offsetHeight / data.height,
-							height: item.offsetHeight
-						})
-					});
-				});
-			});
-		}
-	});
-	
 	//
 	// View: PictureWall
 	// Handle format: landscape/portrait
@@ -162,7 +47,7 @@
 			});
 			this.__loader.render();
 			
-			this.__scroller = new Scroller(this.el, {
+			this.__scroller = new scrollerView(this.el, {
 				collection: this.collection
 			});
 		},
@@ -264,5 +149,5 @@
 	
 	this.PictureWall = PictureWall;
 
-})(_VIEW["base"], _VIEW["loader"], _COLLECTION["base"], _COLLECTION["grid"]);
+})(_VIEW["base"], _VIEW["loader"], _VIEW["scroller"], _COLLECTION["base"], _COLLECTION["grid"]);
 
---
+++ source/javascripts/view/scroller.js
@@ -0,0 +1,123 @@
+(function(baseView) {
+
+	var NAMESPACE = "scroller";
+	
+	var scrollerView = function(el, options) {
+		baseView.apply(this, arguments);
+	}
+	
+	scrollerView.prototype = Object.create(baseView.prototype);
+	
+	_.extend(scrollerView.prototype, {
+		
+		grid: [],
+		
+		initialize: function(el, options) {
+			baseView.prototype.initialize.apply(this, arguments);
+			var _func = this.resize.bind(this);
+			$("body").on("resize", _.debounce(_func, 100));
+		},
+		
+		render: function(html) {
+			if (!this.__content) {
+				$(this.el).html("<div class=scroller></div>");
+				this.__content = $(".scroller", this.el);
+			}
+	
+			var content = this.__content;
+			
+			if (_.isString(html)) {
+				
+				// Append new HTML
+				content.html(html)
+	
+				// Display columns && rows
+				this.resize();
+			}	
+		},
+		
+		resize: function() {
+			var content = this.__content;
+			
+			// Grid is a table with CID references
+			// of models of collection
+			var grid = this.grid;
+			var collection = this.collection;
+	
+			// Remove Previous resize		
+			content.css("width", "auto");
+			
+			var coords = collection.get_item_size({
+				width: window.innerWidth,
+				height: window.innerHeight
+			});
+			
+			var width_max = coords.width * grid.length;
+			var len = width_max / coords.width;
+			
+			// Get Column Value
+			var column_min = Math.ceil(content.width() / coords.width);
+			var columns = column_min;
+			while((len / columns) > 1 && len % columns > 0) {
+				++columns;
+			}
+			var rows = Math.ceil(len / columns);
+			
+			/*
+			if (this._fill === "height") {
+				// Gallery is bigger than window Size
+				// Lets fill it
+				var surface = coords.width * coords.height;
+				var surface_all = surface * len;
+				var surface_win = window.innerHeight * window.innerWidth;
+				if (surface_all > surface_win && row === 1) {
+					columns = column_min;
+					rows = Math.ceil(len / column);
+				}
+			
+				// Add Vertical Alignement
+				var height_min = window.innerHeight;
+				var height = rows * coords.height;
+				this.el.css({
+					"padding": Math.ceil((height_min - height) / 2) + "px 0"
+				});
+			}
+			*/
+			
+			// Force Content.width
+			// to have scroller
+			var width = columns * coords.width;
+			// if (width > width_max) width = width_max;
+			content.width(width);
+			console.log(columns, coords.width, width)
+			
+			// Resize Grid Items
+			// @TODO : add methods
+			_.each(grid, function(data) {
+				_.each(data, function(cid, index) {
+					var item = $('[data-cid=' + cid + ']', this.el);
+					
+					// Resize Container
+					if (!index) {
+						$(item).parent().css(coords);
+					}
+					
+					// Resize Picture
+					var model = _.find(collection.models, function(_model) {
+						return _model.cid == cid;
+					});
+					$(item).css({
+						"background-size": _.template('<%= width %>px <%= height %>px', {
+							width: data.width * item.offsetHeight / data.height,
+							height: item.offsetHeight
+						})
+					});
+				});
+			});
+		}
+	});
+
+	_VIEW[NAMESPACE]  = scrollerView;
+
+})(_VIEW["base"]);
+
