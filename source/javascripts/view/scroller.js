30bd7c92fab0fb44e262fb86ff9c44892bced087
Noëlie <noelieamiot@gmail.com>
2015-03-04 15:12:44 +0100

    FIX : missing specifi case : 'lonely landscape'

--- source/javascripts/view/scroller.js
+++ source/javascripts/view/scroller.js
@@ -92,36 +92,53 @@
 			// to have scroller
 			var width = columns * coords.width;
 			content.width(width);
-			
+            
 			// Resize Grid Items
 			_.each(grid, function(data) {
 				_.each(data, function(cid, index) {
 					var item = $('[data-cid=' + cid + ']', this.el);
-					
-					// Resize Container
-					if (!index) {
-						$(item).parent().css(coords);
-					}
 					
 					// Resize Picture
 					var model = _.find(collection.models, function(_model) {
 						return _model.cid == cid;
 					});
 					
+					// Resize Container
+					if (!index) {
+                        
+                        // When there is only 1 "landscape" picture, 
+                        // container is twice larger than a "portrait"
+                        if (model.format == "landscape" && data.length == 1) {
+    						$(item).parent().css({
+    						    width: coords.width * 2,
+                                height: coords.height
+    						});
+                            item.css({
+                                "background-size": "auto 100%",
+                                "height": coords.height
+                            });
+                            return;
+                        } 
+
+						$(item).parent().css(coords);
+					}
+                    
 					var _height = item.height();
+                    
                     // FIX : clean width a model method (get_img_width, et get_img_height)
 					var _width = (model.img_width || model.width) * _height / (model.img_height || model.height);
 					if (_width < coords.width) {
 						_height *=  coords.width / _width;
 						_width = coords.width;
 					}
-					
-					item.css({
-						"background-size": _.template('<%= width %>px <%= height %>px', {
-							width: Math.ceil(_width),
-							height: Math.ceil(_height)
-						})
-					});
+                    
+                    item.css({
+                        "background-size": _.template('<%= width %>px <%= height %>px', {
+                            width: Math.ceil(_width),
+                            height: Math.ceil(_height)
+                        })
+                    });
+
 				});
 			});
 		}
